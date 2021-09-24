d52ed34a5c03d4635c28d711fa7d6a0b1db8183c
Alireza <alireza.gholami403@gmail.com>
2021-09-25 00:33:16 +0330

    fix: typo in index.js

--- index.js
+++ index.js
@@ -5,18 +5,17 @@ const cors = require('cors');
 
 const port = process.env.PORT || 80;
 
-
-
 const app = express();
 app.use(cors());
 app.options('*', cors());
 
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
-let template = fs.readFileSync(path.resolve('./public/index.html'), 'utf-8');
+const template = fs.readFileSync(path.resolve('./public/index.html'), 'utf-8');
 app.get('/', function (req, res) {
     res.send(template);
 });
 
-app.listen(port);
-console.log("server started on port " + port);
+app.listen(port, () => {
+    console.log("server started on port " + port);
+});
